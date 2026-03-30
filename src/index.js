cacc3161244eff9dabedeefae088d7907c6a453e
ahmadalaik <ahmadalaik.mu@gmail.com>
2026-03-30 19:56:48 +0700

    upload all-features

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { authMiddleware } from "./middleware/auth.middleware.js";
 import { roleMiddleware } from "./middleware/role.middleware.js";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const corsOptions = {
   origin: ["http://localhost:5173"], // Whitelist specific domains
